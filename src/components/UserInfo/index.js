713821fe7daa39acc6bb882cecbee03a05db567a
LahariMamidi <mlahari267@gmail.com>
2025-05-20 19:58:56 +0530

    CP22-Blog-List

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,16 @@
+// Write your JS code here
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info-container">
+    <img
+      className="profle-img"
+      alt="profile"
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+    />
+    <h1 className="heading">Wade Warren</h1>
+    <p className="text">Software Developer at UK</p>
+  </div>
+)
+
+export default UserInfo
